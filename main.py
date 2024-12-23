b44e26e2f10cd08d338ed1d577a9445b2c9df45f
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-23 16:47:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+# grabbing the range from 1 (-1) to 100 (101)
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please use a number larger than 0")
+        quit()
+else:
+    print("Please use a number")
+    quit()
+
+
+random_number = random.randrange(-1, 101)
